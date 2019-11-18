585c7e1a2f62c8074a2f716476285a16674d067a
Vitali Orlioglo <vitalik.orlioglo@gmail.com>
2019-11-18 10:39:53 +0100

    JSF Kundenlogin

---
+++ README.md
@@ -0,0 +1,9 @@
+# JSF Kundenlogin 01
+
+## ohne DB - KundeDummyDAO
+* nur Kunde-Rolle
+* findKundeBy ...
+* storeNewKunde
+
+## TODO
+* logout-Methode (KundeBean)
---
+++ src/dao/KundeMySQLDAO.java
@@ -0,0 +1,19 @@
+package dao;
+
+import model.Kunde;
+
+public class KundeMySQLDAO implements KundeDAO {
+
+	@Override
+	public Kunde findKunde(String usr, String pwd) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean storeNewKunde(Kunde newKunde) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
